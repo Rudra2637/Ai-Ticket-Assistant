7c5238c036d3a2f5ece2b4e82817491d4538b590
Rudra2637 <singhrudra2637@gmail.com>
2025-11-21 11:53:24 +0530

    correction done

--- ai-assistant/routes/ticket.routes.js
+++ ai-assistant/routes/ticket.routes.js
@@ -1,16 +1,7 @@
 import express from 'express'
-import { getUsers, login, logout, signUp, updateUser } from '../controllers/user.controller'
-import { authmiddleWare } from '../middlewares/auth'
+
+import { authmiddleWare } from '../middlewares/auth.js'
 
 const router = express.Router()
-
-router.post("/signup",signUp)
-
-router.post("/login",login)
-
-router.post("/logout",logout)
-
-router.post("/update-User",authmiddleWare,updateUser)
-router.get("/users",authmiddleWare,getUsers)
 
 export default router
--- ai-assistant/routes/user.routes.js
+++ ai-assistant/routes/user.routes.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import { getUsers, login, logout, signUp, updateUser } from '../controllers/user.controller.js'
+import { authmiddleWare } from '../middlewares/auth.js'
 
 const router = express.Router()
+
+router.post("/signup",signUp)
+
+router.post("/login",login)
+
+router.post("/logout",logout)
+
+router.post("/update-User",authmiddleWare,updateUser)
+router.get("/users",authmiddleWare,getUsers)
 
 export default router
